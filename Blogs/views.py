9b40455bda3981f17eea247479afe106a321360e
Power Adapter <1244280607@qq.com>
2025-02-21 05:00:38 +0800

    feat: add urls

--- Blogs/views.py
+++ Blogs/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+from Blogs.models import Post
+
+
 # Create your views here.
+def post_list(request):
+    pass
+
+
+def post_detail(request, pk):
+    pass
+
+
+def links(request):
+    pass

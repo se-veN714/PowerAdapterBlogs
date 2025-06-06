dcca7b955e37f01ff6eb4e7dd417f11ab321b878
Power Adapter <1244280607@qq.com>
2025-06-07 03:10:26 +0800

    fix:重新理解和修改了CBV

--- Blogs/views.py
+++ Blogs/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import get_object_or_404
+from typing import Dict, Any
+
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.views.generic import DetailView, ListView, TemplateView
 
 from Blogs.models import Post, Tag, Category
@@ -7,7 +9,7 @@ from config.models import SideBar
 
 
 # Create your views here.
-class CommonViewMixin:# 不让它继承任何类，而是将这个 Mixin 与有 get_context_data() 方法的视图类一起使用
+class CommonViewMixin:  # 不让它继承任何类，而是将这个 Mixin 与有 get_context_data() 方法的视图类一起使用
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context.update({
@@ -17,9 +19,14 @@ class CommonViewMixin:# 不让它继承任何类，而是将这个 Mixin 与有 
 
 class IndexView(CommonViewMixin, ListView):
     queryset = Post.latest_posts()
-    paginate_by = 10
+    paginate_by = 5
     context_object_name = 'post_list'
     template_name = 'blog/list.html'
+
+
+class PostDetailView(CommonViewMixin, DetailView):
+    model = Post
+    template_name = 'blog/detail.html'
 
 
 class PostListView(ListView):
@@ -29,32 +36,27 @@ class PostListView(ListView):
     template_name = 'blog/list.html'
 
 
-class PostDetailView(CommonViewMixin, DetailView):
-    model = Post
-    queryset = Post.objects.filter(status=Post.STATUS_NORMAL)
-    template_name = 'blog/detail.html'
-
-
 class CategoryView(IndexView):
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, **kwargs) -> Dict[str, Any]:
         context = super().get_context_data(**kwargs)
-        category_id = self.kwargs['category_id']
-        category = get_object_or_404(Category, id=category_id)
+        category_id = self.kwargs.get('category_id')
+        category = get_object_or_404(Category, pk=category_id)
         context.update({
             'category': category,
         })
         return context
 
     def get_queryset(self):
-        queryset = Post.objects.get_queryset()
-        category_id = self.kwargs['category_id']
+        """ 重写 queryset，根据分类过滤 """
+        queryset = super().get_queryset()
+        category_id = self.kwargs.get('category_id')
         return queryset.filter(category_id=category_id)
 
 
 class TagView(IndexView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
-        tag_id = self.kwargs['tag_id']
+        tag_id = self.kwargs.get('tag_id')
         tag = get_object_or_404(Tag, id=tag_id)
         context.update({
             'tag': tag,
@@ -62,9 +64,10 @@ class TagView(IndexView):
         return context
 
     def get_queryset(self):
-        queryset = Post.objects.get_queryset()
-        tag_id = self.kwargs['tag_id']
-        return queryset.filter(tag_id=tag_id)
+        """ 重写 queryset，根据分类过滤 """
+        queryset = super().get_queryset()
+        tag_id = self.kwargs.get('tag_id')
+        return queryset.filter(category_id=tag_id)
 
 
 def links(request):

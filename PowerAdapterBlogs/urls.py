8082b1956092f364c9f60a4c0c05513ff7fd2d01
Power Adapter <1244280607@qq.com>
2025-08-12 03:12:10 +0800

    feat:Use django-debug-toolbar to optimize & debug

--- PowerAdapterBlogs/urls.py
+++ PowerAdapterBlogs/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf import settings
 from django.contrib import admin
 from django.contrib.sitemaps import views as sitemaps_views
 from django.urls import path, include
@@ -42,3 +42,9 @@ urlpatterns = [
     # accounts
     path('accounts/', include(('accounts.urls', 'accounts'), namespace='accounts')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        path("__debug__/", include(debug_toolbar.urls)),
+    ]
